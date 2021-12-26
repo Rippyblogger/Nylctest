638fa56f5ba2129b8734b92a281a797a17ce9512
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2021-12-26 15:20:54 +0400

    setup commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class=" bg-brand-black h-screen w-screen text-white px-6">
+		<div class="flex justify-center items-center h-screen flex-col">
+			<img src="../assets/logo.svg" alt="image-logo" />
+			<h3 class="text-white text-sm my-6 text-center">
+				BUREIKOU - [BOO-REY-KOO] JAPANESE 無礼講 <br> putting everything aside to be
+				yourself
+			</h3>
+
+            <div class="mt-8">
+                <button class="bg-brand-black border-2 border-brand-yellow hover:bg-brand-yellow hover:text-white px-6 py-2">Contact us</button>
+            </div>
+		</div>
+		<div></div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+	name: "Home",
+};
 </script>
+
+<style lang="css" scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+      colors: {
+        brand: {
+          black: "#161616",
+          yellow: "#CDC152"
+        }
+			},
+		},
+	},
+	plugins: [],
+};
