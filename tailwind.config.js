08166c9337785efe6744add829016111840460cc
Boye Adeotan <55053080+Rippyblogger@users.noreply.github.com>
2021-12-28 11:57:04 +0400

    minify css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 module.exports = {
+	purge: ["./public/**/*.html", "./src/**/*.vue"],
 	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			colors: {
 				brand: {
 					black: "#161616",
-					yellow: "#CDC152"
-				}
+					yellow: "#CDC152",
+				},
 			},
 			fontFamily: {
-       			Roboto: ["Roboto", "sans-serif"],
-      		},
+				Roboto: ["Roboto", "sans-serif"],
 			},
+		},
 	},
 	plugins: [],
 };
